b2fb59468df1815c37e74619a89b9490535fce6b
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-01-03 01:27:16 -0300

    Dashboard Next with Shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Sidebar from "@/components/sidebar/Sidebar";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Dashboard Shadcn",
+  description: "Template for Next.js with Tailwind CSS and Shadcn UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={cn(
+          "min-h-screen bg-gray-100 font-sans antialiased",
+          geistSans.variable,
+          geistMono.variable
+        )}
+      >
+        <Sidebar />
+        {children}
+      </body>
+    </html>
+  );
+}
